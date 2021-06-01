dff4de766dd6c5661c683e69a6c5f3a2f475a7f6
Chris Joo <chrisj1225@gmail.com>
2021-06-01 11:55:22 -0400

    Set up & test webpack, create script files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles/index.scss";
+console.log("webpack is working properly");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  
+})
