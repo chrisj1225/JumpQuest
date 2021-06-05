65020a579c1f444263b2ebe6e921f604e7f60ee3
Chris Joo <chrisj1225@gmail.com>
2021-06-05 01:09:46 -0400

    Edit welcome modal message

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -5,19 +5,23 @@ const welcomeModal = (ctx, gameStart, GAME_WIDTH, GAME_HEIGHT) => {
     ctx.fillStyle = "lightslategrey";
     ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     ctx.fillStyle = "snow";
-    ctx.font = 'bold 40px arial';
-    ctx.fillText("Welcome to Jump Quest!", 275, 50);
+    ctx.font = 'bold 50px arial';
+    ctx.fillText("Welcome to Jump Quest!", 225, 60);
     ctx.font = 'bold 30px arial';
     ctx.fillText("Beemo          is lost!", 75, 150);
     ctx.fillText("Help Finn        navigate across the platforms to find him.", 75, 225);
     ctx.fillText("Be sure to dodge the flying obstacles using crouch or jump.", 75, 300);
     ctx.fillText("If you get hit, you may have to start from an earlier position.", 75, 375);
+
+    ctx.font = 'bold 20px arial';
+    ctx.fillText("- Horizontally moving balls will push you in their direction", 100, 410);
+    ctx.fillText("- Vertically moving balls will knock you off of the platform", 100, 445);
+    
+    ctx.font = 'bold 30px arial';
+    ctx.fillText("Good luck!!!", 75, 550);
     
     ctx.font = 'bold 20px arial';
     ctx.fillText("(If your screen height is smaller than 865 pixels, please consider zooming out to 80-90%)", 75, 600);
-
-    ctx.font = 'bold 30px arial';
-    ctx.fillText("Good luck!!!", 75, 500);
     
     ctx.font = 'bold 40px arial';
     ctx.fillText("Press ENTER to begin!", 275, 700);
